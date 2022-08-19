87a86f3376576626c47aa0c390c45ac9205445cb
memosk8 <memolopez9363@gmail.com>
2022-08-19 17:13:28 -0500

    testing views

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', content: 'Express es una librería de nodejs para manejar solicitudes y respuestas HTTP'});
 });
 
 module.exports = router;
