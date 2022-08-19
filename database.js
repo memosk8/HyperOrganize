75c6c4bd32bdac2306a1fde83016f1b7b168e6e3
memosk8 <memolopez9363@gmail.com>
2022-08-19 17:11:40 -0500

    x

--- database.js
+++
@@ -1,5 +0,0 @@
-// conexion a mongodb
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://memosk8:Password123@cluster0.nuhhssx.mongodb.net/HyperOrganize?retryWrites=true&w=majority')
-  .then(() => console.log("\n-- Base de datos conectada --\n"))
-  .catch(err => console.log(err));
